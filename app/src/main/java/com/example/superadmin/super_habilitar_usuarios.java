95c82b9099fc1badff29927756089a7a32395e97
David Llanos <david.llanos@pucp.edu.pe>
2024-09-19 13:11:18 -0500

    vista 5

---
+++ app/src/main/java/com/example/superadmin/super_habilitar_usuarios.java
@@ -0,0 +1,14 @@
+package com.example.superadmin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class super_habilitar_usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_habilitar_usuarios);
+    }
+}
