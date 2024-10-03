02e0c881d84ca72eecd057defa9e1076e0eb7339
Juan Pablo <a20180951@pucp.edu.pe>
2024-10-03 00:03:17 -0500

    menu desplegable usuario

--- app/src/main/java/com/example/superadmin/HomeActivity.java
+++ app/src/main/java/com/example/superadmin/HomeActivity.java
@@ -10,12 +10,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 public class HomeActivity extends AppCompatActivity {
     ConstraintLayout toolbar;
     ImageButton btnCar;
     ImageButton btnProfile;
+    ImageButton btnDesplegable;
+    DrawerLayout drawerLayout;
+    ConstraintLayout desplegable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +31,29 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });
         toolbar = findViewById(R.id.toolbar_home);
-        btnCar = toolbar.findViewById(R.id.btn_shopping_bag);
-        btnProfile = toolbar.findViewById(R.id.btn_profile);
 
+
+        btnCar = toolbar.findViewById(R.id.btn_shopping_bag);
+         // btnProfile = toolbar.findViewById(R.id.btn_profile);
+        btnDesplegable= toolbar.findViewById(R.id.btn_menu);
+        //drawerLayout = desplegable.findViewById(R.id.draweLayout);
+
+        btnDesplegable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, UserDesplegableActivity.class));
+            }
+        });
+
+/*
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
             }
         });
+*/
+
 
         btnCar.setOnClickListener(new View.OnClickListener() {
             @Override
