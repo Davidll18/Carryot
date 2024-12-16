87ffd54e54e2fbd7e4cd1f53b9b011d4a1555dbc
Juan Pablo <a20180951@pucp.edu.pe>
2024-12-15 22:45:00 -0500

    ImagenRestaurante user

--- app/src/main/java/com/example/superadmin/adapters/UserRestaurantAdapter.java
+++ app/src/main/java/com/example/superadmin/adapters/UserRestaurantAdapter.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.superadmin.R;
 import com.example.superadmin.dtos.RestaurantDTO;
 
@@ -39,6 +41,16 @@ public class UserRestaurantAdapter extends RecyclerView.Adapter<UserRestaurantAd
 
         // Asignar valores a las vistas
         holder.restaurantName.setText(restaurant.getNombreRestaurante());
+        holder.calificacion.setText(restaurant.getRateRest());
+        holder.categoria.setText(restaurant.getCategoria());
+        holder.costodelivery.setText(restaurant.getCostoDelivery());
+        holder.tiempo.setText(restaurant.getTiempoEspera());
+
+        // Cargar imagen con Glide
+        Glide.with(holder.itemView.getContext())
+                .load(restaurant.getImageUrl()) // URL de la imagen desde Firebase
+                .placeholder(R.drawable.logo)   // Imagen por defecto mientras carga
+                .into(holder.imageProduct);     // Referencia al ImageView
 
         // Imprimir el UID del restaurante en el log para verificar
         if (restaurant.getUidCreacion() != null) {
@@ -77,6 +89,7 @@ public class UserRestaurantAdapter extends RecyclerView.Adapter<UserRestaurantAd
     public static class RestaurantViewHolder extends RecyclerView.ViewHolder {
         TextView restaurantName,calificacion,categoria,costodelivery,tiempo;
         CardView cardView;
+        ImageView imageProduct;
 
         public RestaurantViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -85,7 +98,8 @@ public class UserRestaurantAdapter extends RecyclerView.Adapter<UserRestaurantAd
             categoria = itemView.findViewById(R.id.categoria);
             costodelivery = itemView.findViewById(R.id.text_delivery_cost);
             tiempo = itemView.findViewById(R.id.text_time);
-            cardView = itemView.findViewById(R.id.card_view_restaurant); // Asegúrate de que tu diseño tiene un CardView con este ID
+            cardView = itemView.findViewById(R.id.card_view_restaurant);
+            imageProduct = itemView.findViewById(R.id.img_menu_item);// Asegúrate de que tu diseño tiene un CardView con este ID
         }
     }
 }
